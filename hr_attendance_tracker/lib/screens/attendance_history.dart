c1f110a446e46c599e7b07bb8b4531282284eebf
hidayattresnadi <hidayattresnadi64@gmail.com>
2025-08-13 13:25:30 +0700

    modified header attandance log

--- hr_attendance_tracker/lib/screens/attendance_history.dart
+++ hr_attendance_tracker/lib/screens/attendance_history.dart
@@ -4,12 +4,14 @@ import 'package:intl/intl.dart';
 
 class AttendanceRecord {
   final String date;
+  final String day;
   final String checkIn;
   final String checkOut;
   final String status;
 
   AttendanceRecord({
     required this.date,
+    required this.day,
     required this.checkIn,
     required this.checkOut,
     required this.status,
@@ -17,44 +19,58 @@ class AttendanceRecord {
 }
 
 final List<AttendanceRecord> dummyRecords = [
+  // AttendanceRecord(
+  //   date: 'date',
+  //   day: '',
+  //   checkIn: 'check in',
+  //   checkOut: 'check out',
+  //   status: 'status',
+  // ),
   AttendanceRecord(
     date: 'Aug 1, 2022',
+    day: 'Monday',
     checkIn: '08:00',
     checkOut: '17:00',
     status: 'Present',
   ),
   AttendanceRecord(
     date: 'Aug 2, 2022',
+    day: 'Monday',
     checkIn: '08:05',
     checkOut: '17:10',
     status: 'Present',
   ),
   AttendanceRecord(
     date: 'Aug 3, 2022',
+    day: 'Monday',
     checkIn: '08:15',
     checkOut: '16:50',
     status: 'Present',
   ),
   AttendanceRecord(
     date: 'Aug 4, 2022',
+    day: 'Monday',
     checkIn: '-',
     checkOut: '-',
     status: 'Absent',
   ),
   AttendanceRecord(
     date: 'Aug 5, 2022',
+    day: 'Monday',
     checkIn: '08:10',
     checkOut: '17:00',
     status: 'Present',
   ),
   AttendanceRecord(
     date: 'Aug 6, 2022',
+    day: 'Monday',
     checkIn: '08:20',
     checkOut: '17:30',
     status: 'Present',
   ),
   AttendanceRecord(
     date: 'Aug 7, 2022',
+    day: 'Monday',
     checkIn: '-',
     checkOut: '-',
     status: 'Absent',
@@ -65,66 +81,131 @@ class AttendanceItem extends StatelessWidget {
   final AttendanceRecord record;
   const AttendanceItem({super.key, required this.record});
 
+  static const double dateWidth = 90;
+  static const double smallColWidth = 80;
+  static const double statusWidth = 90;
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const Divider(height: 24, thickness: 2),
-        ListTile(
-          contentPadding: EdgeInsets.symmetric(horizontal: 8),
-          title: Row(
-            children: [
-              // Date and Day
-              Expanded(
-                flex: 3,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      record.date,
-                      style: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 12,
-                      ),
-                    ),
-                    const SizedBox(height: 2),
-                    const Text("Monday", style: TextStyle(fontSize: 12)),
-                  ],
+    double screenWidth = MediaQuery.of(context).size.width;
+    double fontSize = screenWidth < 390 ? 12 : 14;
+    return ListTile(
+      contentPadding: const EdgeInsets.symmetric(horizontal: 8),
+      title: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          // DATE (column supaya bisa ada date + day)
+          Expanded(
+            flex: 1,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              // di Row
+              crossAxisAlignment: CrossAxisAlignment.center,
+
+              children: [
+                Text(
+                  record.date,
+                  style: TextStyle(
+                    fontSize: record.day.isNotEmpty ? 13 : 16,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-              // Check-in
-              Expanded(
-                flex: 2,
-                child: Text(record.checkIn, textAlign: TextAlign.center),
-              ),
-              // Check-out
-              Expanded(
-                flex: 2,
-                child: Text(record.checkOut, textAlign: TextAlign.center),
-              ),
-              // Status (Icon + Text)
-              Expanded(
-                flex: 2,
-                child: Column(
-                  children: [
-                    Icon(
-                      record.status == 'Present'
-                          ? Icons.check_circle
-                          : Icons.cancel,
-                      size: 18,
-                      color: record.status == 'Present'
-                          ? Colors.green
-                          : Colors.red,
-                    ),
-                    const SizedBox(height: 4),
-                    Text(record.status, style: const TextStyle(fontSize: 12)),
-                  ],
-                ),
-              ),
-            ],
+                if (record.day.isNotEmpty) const SizedBox(height: 4),
+                if (record.day.isNotEmpty)
+                  Text(record.day, style: const TextStyle(fontSize: 12)),
+              ],
+            ),
           ),
-        ),
-      ],
+
+          // spacing antar kolom (opsional)
+          const SizedBox(width: 8),
+
+          // CHECK IN
+          Expanded(
+            flex: 1,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              // di Row
+              crossAxisAlignment: CrossAxisAlignment.center,
+
+              children: [
+                Text(
+                  record.checkIn,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontWeight: record.day.isNotEmpty
+                        ? FontWeight.normal
+                        : FontWeight.bold,
+                  ),
+                ),
+                if (record.day.isNotEmpty) const SizedBox(height: 4),
+                if (record.day.isNotEmpty)
+                  Text('', style: const TextStyle(fontSize: 12)),
+              ],
+            ),
+          ),
+
+          const SizedBox(width: 8),
+
+          // CHECK OUT
+          Expanded(
+            flex: 1,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              // di Row
+              crossAxisAlignment: CrossAxisAlignment.center,
+
+              children: [
+                Text(
+                  record.checkOut,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontWeight: record.day.isNotEmpty
+                        ? FontWeight.normal
+                        : FontWeight.bold,
+                  ),
+                ),
+                if (record.day.isNotEmpty) const SizedBox(height: 4),
+                if (record.day.isNotEmpty)
+                  Text('', style: const TextStyle(fontSize: 12)),
+              ],
+            ),
+          ),
+
+          const SizedBox(width: 8),
+
+          // STATUS (SELALU Column supaya struktur sama di header & row)
+          Expanded(
+            flex: 1,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                if (record.status !=
+                    'status') // kalau bukan header, tampilkan icon
+                  Icon(
+                    record.status == 'Present'
+                        ? Icons.check_circle
+                        : Icons.cancel,
+                    size: 18,
+                    color: record.status == 'Present'
+                        ? Colors.green
+                        : Colors.red,
+                  ),
+                if (record.status != 'status') const SizedBox(height: 4),
+                Text(
+                  record.status,
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: record.day.isNotEmpty
+                        ? FontWeight.normal
+                        : FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
@@ -242,21 +323,26 @@ Widget attendanceScreenBody() {
         // Attendance List Card
         Card(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               // Table Header
               Padding(
-                padding: const EdgeInsets.only(top: 20, left: 8, right: 8),
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 8.0,
+                  vertical: 20,
+                ),
                 child: Row(
                   children: const [
                     Expanded(
-                      flex: 3,
+                      flex: 1,
                       child: Text(
                         'Date',
                         style: TextStyle(fontWeight: FontWeight.bold),
+                        textAlign: TextAlign.center,
                       ),
                     ),
                     Expanded(
-                      flex: 2,
+                      flex: 1,
                       child: Text(
                         'In Time',
                         style: TextStyle(fontWeight: FontWeight.bold),
@@ -264,7 +350,7 @@ Widget attendanceScreenBody() {
                       ),
                     ),
                     Expanded(
-                      flex: 2,
+                      flex: 1,
                       child: Text(
                         'Out Time',
                         style: TextStyle(fontWeight: FontWeight.bold),
@@ -272,7 +358,7 @@ Widget attendanceScreenBody() {
                       ),
                     ),
                     Expanded(
-                      flex: 2,
+                      flex: 1,
                       child: Text(
                         'Status',
                         style: TextStyle(fontWeight: FontWeight.bold),
@@ -282,16 +368,22 @@ Widget attendanceScreenBody() {
                   ],
                 ),
               ),
-
-              const SizedBox(height: 8),
+              // const SizedBox(height: 40),
+              Divider(thickness: 1),
               // List attendance
-              ListView.builder(
+              ListView.separated(
                 shrinkWrap: true,
-                physics:
-                    const NeverScrollableScrollPhysics(), // Biar scroll-nya dari luar
+                physics: const NeverScrollableScrollPhysics(),
                 itemCount: dummyRecords.length,
                 itemBuilder: (context, index) {
                   return AttendanceItem(record: dummyRecords[index]);
+                },
+                separatorBuilder: (context, index) {
+                  return const Divider(
+                    thickness: 1,
+                    color: Colors.grey,
+                    height: 20, // jarak vertikal
+                  );
                 },
               ),
             ],
